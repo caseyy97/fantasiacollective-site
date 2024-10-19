8e3910aeb3269ef65da8bbe5eec00d2dc9e7ddb3
Casey <casey97@protonmail.com>
2024-10-19 13:44:25 +1000

    fix db stuff

--- fantasiasite/settings.py
+++ fantasiasite/settings.py
@@ -20,7 +20,6 @@ SITE_DIR = BASE_DIR / 'info'
 MEDIA_ROOT = SITE_DIR / 'media'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
 
 SECRET_KEY = os.environ.get(
     "DJANGO_SECRET_KEY",
@@ -150,13 +149,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-if IS_CAPROVER_APP == True:
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-else:
-    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
-    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-
 S3_USE_SIGV4 = True 
 AWS_IS_GZIPPED = True
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
@@ -165,6 +157,15 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL')
 AWS_S3_CUSTOM_DOMAIN = os.environ.get('AWS_S3_CUSTOM_DOMAIN')
 AWS_S3_HOST = AWS_S3_ENDPOINT_URL
+
+if IS_CAPROVER_APP == True:
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATIC_URL = AWS_S3_CUSTOM_DOMAIN
+else:
+    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+    STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
